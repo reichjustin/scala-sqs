7c5487c2aa5890c02395209ac4f8d54a3497db2b
Justin <sql@Justins-MacBook-Pro.local>
2016-09-07 15:42:50 -0700

    added RabbitConnection object

--- hi.scala
+++ hi.scala
@@ -1,15 +1,19 @@
 object Starter extends App {
 
-  class Justin(age: Int) extends Person(age: Int) {
-      val name = "Justin"
-      val a = age
+  /*
+   * This is the RabbitConnection Object
+   * That will manage our queue connection
+   */
+  object RabbitConnection {
+    val server = "http://localhost"
+    val port = 15672
+    val connectionString = s"$server/$port"
+
+    def Initialize() = {
+    //  val connection = new ConnectionFactory()
+    }
   }
 
-  var p = new Justin(30)
-  println(p.name)
-  println(p.a)
-}
-
-abstract class Person(age: Int) {
-  val name: String
+  //initialize that RabbitMQ connection
+  RabbitConnection.Initialize()
 }
