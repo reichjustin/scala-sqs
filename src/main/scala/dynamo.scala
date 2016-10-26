97cbfc44ad440fbfa8cb5ca058faf4783756abfd
Justin Reich <reich.justin@gmail.com>
2016-10-26 10:47:24 -0700

    another concourse test

--- src/main/scala/dynamo.scala
+++ src/main/scala/dynamo.scala
@@ -7,7 +7,7 @@ object dynamo {
 
  //this is a test - haha
   def Save(msg: sqs.Message) = {
-    //this expects a dynamo config to exist
+    //this expects a dynamo config to exist another change
     implicit val dynamoDB = DynamoDB.at(Region.US_WEST_2)
     val table: Table = dynamoDB.table("sqs-msgs").get
 
