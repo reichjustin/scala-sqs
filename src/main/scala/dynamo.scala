f4201333d5e2cd844e6e569f133155ed5f0cb4da
Justin Reich <reich.justin@gmail.com>
2016-10-26 10:44:57 -0700

    Update dynamo.scala

--- src/main/scala/dynamo.scala
+++ src/main/scala/dynamo.scala
@@ -5,7 +5,7 @@ import awscala._, dynamodbv2._
   */
 object dynamo {
 
-
+ //this is a test
   def Save(msg: sqs.Message) = {
     //this expects a dynamo config to exist
     implicit val dynamoDB = DynamoDB.at(Region.US_WEST_2)
